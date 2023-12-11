208df655e4a87bc9443440eda8c844bbd4737c0b
majaperic-ts <maja.peric@zendesk.com>
2023-12-11 10:30:56 +0100

    1 oop - BankAccount excersise

--- 01_OOP/app.js
+++ 01_OOP/app.js
@@ -3,3 +3,26 @@ console.log("hello")
 function test() {
     return "test"
 }
+
+class BankAccount {
+    constructor(accountNumber, accountHolder, balance = 0) {
+        this.accountNumber = accountNumber
+        this.accountHolder = accountHolder
+        this.balance = balance
+    }
+
+    deposit(amt) {
+        this.balance = this.balance + amt
+        console.log('balance is now ', this.balance)
+    }
+
+    withdraw(amt) {
+        if (this.balance < amt) {
+            console.log("You don't have enough balance, balance is ", this.balance )
+            return
+        }
+
+        this.balance = this.balance - amt
+        console.log('balance is now ', this.balance)
+    }
+}
