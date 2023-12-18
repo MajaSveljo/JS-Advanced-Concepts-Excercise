7cea21364e1e0bf47fb18bbae7f5b78276d1cfe8
majaperic-ts <maja.peric@zendesk.com>
2023-12-18 12:07:32 +0100

    2 oop private properties, methods and static initialization blocks

--- 02_OOPNewFeatures/app.js
+++ 02_OOPNewFeatures/app.js
@@ -14,3 +14,39 @@ class User {
         this.lastName = last
     }
 }
+
+class Circle {
+    #radious;
+
+    constructor(radious) {
+        this.#radious = radious
+    }
+
+    getRadious() {
+        return this.#radious
+    }
+}
+
+const myCircle = new Circle(10)
+// this will work in devtools, but it doesn't really work in a program
+// console.log(myCircle.#radious)
+
+class MyClass {
+    #privateMethod() {
+        console.log("Private method called")
+    }
+
+    publicMethod() {
+        this.#privateMethod();
+    }
+}
+
+const myClass = new MyClass();
+myClass.publicMethod();
+
+// static initialization blocks
+class MyClassStatic {
+    static {
+        console.log('class is loaded')
+    }
+}
